0c604cabef48e0995b1052395a54572ad03d5eb2
Tkmt-U <umitakamatsu@gmail.com>
2020-11-11 14:35:33 +0000

    Update Commit(マイページ完成)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "Update successful"
+      redirect_to users_mypage_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email, :profile_image)
   end
 end
