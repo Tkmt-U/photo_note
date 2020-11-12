c8ec1aac987d2515fd3447b1cd14c69b6a7dabbe
Tkmt-U <umitakamatsu@gmail.com>
2020-11-12 08:44:32 +0000

    Update Commit(お気に入り機能完成)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,21 @@ class UsersController < ApplicationController
 
   def mypage
     @user = current_user
+    # 以下お気に入りされた数の算出の記述
+    photos = Photo.where(user_id: current_user.id) # そのユーザの投稿を全て検索
+    @favorite_sum = 0
+    photos.each do |photo|
+      favorite_count = photo.favorites.count # その投稿一つ一つにお気に入りがどれだけついているか出す
+      @favorite_sum += favorite_count # 投稿一つ一つのお気に入りを合計する
+    end
+    # お気に入りされた数の算出ここまで
   end
 
   def edit
     @user = current_user
+    unless params[:id].to_i == current_user.id
+      redirect_to users_mypage_path
+    end
   end
 
   def update
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,4 +7,8 @@ class Photo < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :camera, presence: true
+  
+  def favorited?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
