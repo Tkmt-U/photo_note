46ef9144e7391025550afd90599e523ea16d4c47
Tkmt-U <niseganr@yahoo.co.jp>
2020-11-17 04:59:29 +0000

    Update Commit(add Refile.secret_key)

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '40557d9b5922ea74f798600bb6d27bb8327d9058fba4f8a767d6780ba8b8629d380f2ee6fa850cfcb23e02ffa3015fb42f2daf6c6654a73ce3abd6f41816a348'
