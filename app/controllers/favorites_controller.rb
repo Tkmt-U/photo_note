0e8cc13c6140b887c7165e141b5f8a5823d2a5c0
Tkmt-U <niseganr@yahoo.co.jp>
2020-11-18 02:12:37 +0000

    Fix Commit(fix favorited?)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,6 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @photo = Photo.find(params[:photo_id])
     favorite = Favorite.new(photo_id: @photo.id)
